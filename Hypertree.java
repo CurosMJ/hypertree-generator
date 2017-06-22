ca6d0ae829aa09b8db0aca81e8ebb9c74caca399
Curos <curos@curos.in>
2017-06-22 14:29:29 -0400

    add support for basic queries

--- Hypertree.java
+++ Hypertree.java
@@ -10,6 +10,49 @@ class Hypertree {
 		this.indent = indent;
 	}
 
+	public Hypertree prime()
+	{
+		NodeList children = root.getChildNodes();
+		Node first = children.item(0);
+
+		this.root = first;
+
+		return this;
+	}
+
+	public Hypertree head(int n)
+	{
+		NodeList children = root.getChildNodes();
+
+		int i = children.getLength();
+		while (i > n) {
+			root.removeChild(children.item(children.getLength()-1));
+			i = children.getLength();
+		}
+
+		return this;
+	}
+
+	public Hypertree tail(int n)
+	{
+		NodeList children = root.getChildNodes();
+
+		int i = children.getLength();
+		while (i > n) {
+			root.removeChild(children.item(0));
+			i = children.getLength();
+		}
+		
+		return this;
+	}
+
+	public Hypertree simpleTree(int n)
+	{
+		head(n);
+		tail(1);
+		return this;
+	}
+
 	public void print()
 	{
 		printIndent(indent);
@@ -17,9 +60,11 @@ class Hypertree {
 		System.out.print("[Tag: "+root.getNodeName());
 
 		NamedNodeMap attributes = root.getAttributes();
-		for (int i = 0; i < attributes.getLength(); i++) {
-			Node attribute = attributes.item(i);
-			System.out.print(", "+attribute.getNodeName()+": "+attribute.getNodeValue());
+		if (attributes != null) {
+			for (int i = 0; i < attributes.getLength(); i++) {
+				Node attribute = attributes.item(i);
+				System.out.print(", "+attribute.getNodeName()+": "+attribute.getNodeValue());
+			}	
 		}
 
 		NodeList children = root.getChildNodes();
--- HypertreeGenerator.java
+++ HypertreeGenerator.java
@@ -1,22 +1,44 @@
 import java.io.*;
+import java.util.*;
 import org.w3c.dom.*;
 import javax.xml.parsers.*;
 
 class HypertreeGenerator {
 	public static void main(String args[]) throws Exception
 	{
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder builder = factory.newDocumentBuilder();
+		DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
 
-		FileInputStream file = new FileInputStream(args[0]);
+		Scanner sc = new Scanner(new File(args[0]));
 
-		Document document = builder.parse(file);
+		StringBuilder file = new StringBuilder();
+
+		while (sc.hasNextLine())  {
+			file.append(sc.nextLine());
+		}
+
+		ByteArrayInputStream input =  new ByteArrayInputStream(file.toString().replaceAll(">\\s*<", "><").getBytes("UTF-8"));
+		
+		Document document = builder.parse(input);
 
 		Element root = document.getDocumentElement();
 
 		//System.out.println("-----------------------------");
 
 		Hypertree tree = new Hypertree(root, 0);
+
+		if (args.length > 1) {
+			String operation = args[1];
+			int param = 1;
+			if (args.length == 3) {
+				param = Integer.parseInt(args[2]);
+			}
+
+			if (operation.equals("prime")) tree.prime();
+			if (operation.equals("head")) tree.head(param);
+			if (operation.equals("tail")) tree.tail(param);
+			if (operation.equals("simpleTree")) tree.simpleTree(param);
+		}
+
 		tree.print();
 
 		//System.out.println("\n-----------------------------");
